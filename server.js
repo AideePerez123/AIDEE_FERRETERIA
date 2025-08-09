93ae8a8083c338747d45bba37dcee21b1bb1e5e3
AideePerez123 <perezaidee@umes.edu.gt>
2025-08-08 18:06:24 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './routes/index.route.js';
+
+const app = express();
+const port = 3001;
+app.use(express.json());
+
+app.use('/api', routes);
+
+app.listen(port, () => console.log(`Servidor iniciado en puerto ${port}`));
